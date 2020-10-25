832bd0e9f350024692b31d01a6a1fa1fc9f9d18f
Kamil <kogol99@gmail.com>
2020-10-26 00:09:02 +0100

    add zad 2, 3, 4, 5

--- L3/dotNet_Lista3/dotNet_Lista3/Program.cs
+++ L3/dotNet_Lista3/dotNet_Lista3/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Linq;
 
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using System.Collections;
+using static System.Array;
+
+
 namespace dotNet_Lista3
 {
     class Zad1 { 
@@ -112,14 +119,125 @@ namespace dotNet_Lista3
         }
     }
 
+    class Zad2
+    {
+        public void stworz()
+        {
+            Console.WriteLine("Podaj imię");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko");
+            string nazwisko = Console.ReadLine();
+            string read_str;
+            int wiek;
+            do
+            {
+                Console.WriteLine("Podaj wiek");
+                read_str = Console.ReadLine();
+            } while (!int.TryParse(read_str, out wiek));
+
+            int placa;
+            do
+            {
+                Console.WriteLine("Podaj płacę");
+                read_str = Console.ReadLine();
+            } while (!int.TryParse(read_str, out placa));
+
+
+            (string, string, int, int) krotka= (imie, nazwisko, wiek, placa);
+            var krotkaAnonimowa = (imie, nazwisko, wiek, placa);
+
+            wypiszZKrokitSposob1(krotka);
+            wypiszZKrokitSposob2(krotka);
+            wypiszZKrokitSposob3(krotka);
+            wypiszZKrokitSposob1(krotkaAnonimowa);
+            wypiszZKrokitSposob2(krotkaAnonimowa);
+            wypiszZKrokitSposob3(krotkaAnonimowa);
+
+        }
+
+        void wypiszZKrokitSposob1((string, string, int, int) krotka)
+        {
+            Console.WriteLine($"Pan/Pani {krotka.Item1} {krotka.Item2} mając {krotka.Item3} lat zarabia {krotka.Item4} zł miesięcznie.");
+        }
+
+        void wypiszZKrokitSposob2((string name, string surname, int age, int money) krotka)
+        {
+            Console.WriteLine($"Pan/Pani {krotka.name} {krotka.surname} mając {krotka.age} lat zarabia {krotka.money} zł miesięcznie.");
+        }
+
+        void wypiszZKrokitSposob3((string name, string surname, int age, int money) krotka)
+        {
+            (string name, string surname, int age, int money) = krotka;
+            Console.WriteLine($"Pan/Pani {name} {surname} mając {age} lat zarabia {money} zł miesięcznie.");
+        }
+    }
+
+    class Zad3
+    {
+        public void stworz()
+        {
+            int @class = 16;
+            Console.WriteLine($"Urodziłem się {@class}. dnia miesiąca.");
+        }
+    }
+
+    class Zad4
+    {
+        public void stworz()
+        {
+            int[] array = { 4, 5, 7, 2, 5, 8, 0, 1, 4, 8, 20, 35, 735, 446, 3 };
+            Wydrukuj(array);
+
+            Console.WriteLine("\nKopiowanie tablicy: ");
+            int[] copyArray = new int[array.Length];
+            Copy(array, copyArray, array.Length);
+            Wydrukuj(copyArray);
+
+            Console.WriteLine("\nWyszukiwanie indexu elementu wiekszego od 6: ");
+            Console.WriteLine(FindIndex(array, elem => elem > 6));
+
+            Console.WriteLine("\nOdwrocenie tablicy: ");
+            Reverse(array);
+            Wydrukuj(array);
+
+            Console.WriteLine("\nPosortowanie tablicy: ");
+            Sort(array);
+            Wydrukuj(array);
+
+            Console.WriteLine("\nUstawienie wartości 999 na indexie 4 : ");
+            array.SetValue(999, 4);
+            Wydrukuj(array);
+        }
+
+        void Wydrukuj(int[] array)
+        {
+            Console.WriteLine("Zawartość tablicy: ");
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            Zad1 zadanie = new Zad1();
-            zadanie.stworz();
+            //Zad1 zadanie = new Zad1();
+            //zadanie.stworz();
 
+            //Zad2 zadanie2 = new Zad2();
+            //zadanie2.stworz();
+
+            //Zad3 zadanie3 = new Zad3();
+            //zadanie3.stworz();
+
+            //Zad4 zadanie4 = new Zad4();
+            //zadanie4.stworz();
+
+            //Zadanie 5 zostało umieszczone w zadaniu 2.
         }
     }
 }
